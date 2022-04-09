84b89d7b505881a46283812d247139825a1a899d
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-04-09 17:21:11 +0300

    fixture fixed

--- src/DataFixtures/QuoteFixture.php
+++ src/DataFixtures/QuoteFixture.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Quote;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class QuoteFixture extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private $faker;
 
+    public function __contruct()
+    {
+        $this->faker = Factory::create();
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $manager->persist($this->getQuote());
+        }
         $manager->flush();
+    }
+
+    private function getQuote()
+    {
+        return new Quote(
+            $this->faker->sentence(10),
+            $this->faker->name(),
+            $this->faker->year()
+        );
     }
 }
